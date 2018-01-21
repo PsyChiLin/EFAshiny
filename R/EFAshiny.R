6ce918ec65ef66ac7df0d2329fd2ae8b211946ba
Fish <user@Userde-MacBook-Pro.local>
2018-01-21 21:38:18 +0800

    package version

---
+++ R/EFAshiny.R
@@ -0,0 +1,13 @@
+#' EFAshiny is an user-friendly shiny application for exploratory factor analysis
+#' 
+#' @import shiny
+#' @import devtools
+#' @export
+#' @seealso \url{https://github.com/PsyChiLin/EFAshiny}
+#' @examples
+#' if (interactive()) {
+#'   EFAshiny()
+#' }
+EFAshiny <- function() {
+  shiny::runApp()
+}
